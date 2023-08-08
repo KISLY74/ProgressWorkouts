aa084a9cd4b73c83ab9ac5100de904d27bcffe60
KISLY74 <kisly.nikita@mail.ru>
2023-08-08 10:34:03 +0300

    addition approache in exercises block is work

--- components/Approaches.js
+++ components/Approaches.js
@@ -1,50 +1,56 @@
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import themes from "../config/themes"
 import Approache from "./Approache"
 import { useContext, useState } from "react"
 import { observer } from "mobx-react-lite"
-// import AntIcon from "react-native-vector-icons/AntDesign"
 import FontAwIcon from "react-native-vector-icons/FontAwesome"
 import { Button } from "@rneui/themed"
 import { Context } from "../screens/MainScreen/MainScreen"
-import EntypoIcon from "react-native-vector-icons/Entypo"
+import Cancelcon from "react-native-vector-icons/MaterialIcons"
+import AntDesignIcon from "react-native-vector-icons/AntDesign"
+import ModalApproache from "./ModalApproache"
 
 const Approaches = ({ exercise, approaches }) => {
   const { data } = useContext(Context)
+  const [visible, setVisible] = useState(false)
 
   return <View style={styles.container}>
     <Text style={styles.title}>{exercise}</Text>
-    <EntypoIcon
+    {data.isFormatting && <TouchableOpacity
       style={styles.menu}
-      name="dots-three-vertical"
-      size={25}
-      color={themes.first.colors.rare} />
-    {
-      approaches && approaches.map((appr, ind) =>
-        <Approache
-          key={JSON.stringify(appr) + ind}
-          appr={appr}
-          ind={ind} />)
-    }
+      onPress={() => data.setIsFormatting(false)}>
+      <Cancelcon
+        name="cancel"
+        size={35}
+        color={themes.first.colors.rare} />
+    </TouchableOpacity>}
+    {approaches && approaches.map((appr, ind) =>
+      <Approache
+        key={JSON.stringify(appr) + ind}
+        appr={appr}
+        ind={ind} />)}
     <View style={{ flexDirection: 'row', width: '100%' }}>
-      {/* <Button
-        containerStyle={{ width: '50%' }}
-        buttonStyle={{ backgroundColor: themes.first.colors.veryDark }}
-        icon={<AntIcon
-          name="edit"
-          color={themes.first.colors.rare}
-          size={25}
-          style={{ textAlign: 'center', height: 25 }} />} /> */}
-      {data.isFormatting && <Button
-        containerStyle={{ width: '100%' }}
-        buttonStyle={{ backgroundColor: themes.first.colors.veryDark }}
-        icon={<FontAwIcon
-          name="trash-o"
-          color={themes.first.colors.rare}
-          size={25}
-          style={{ textAlign: 'center', height: 25 }} />} />} 
+      {data.isFormatting ?
+        <Button
+          containerStyle={{ width: '100%' }}
+          buttonStyle={{ backgroundColor: themes.first.colors.veryDark }}
+          icon={<FontAwIcon
+            name="trash-o"
+            color={themes.first.colors.rare}
+            size={26}
+            style={{ textAlign: 'center', height: 26 }} />} />
+        : <Button
+          containerStyle={{ width: '100%' }}
+          buttonStyle={{ backgroundColor: themes.first.colors.veryDark }}
+          onPress={() => setVisible(true)}
+          icon={<AntDesignIcon
+            name="pluscircle"
+            color={themes.first.colors.rare}
+            size={26}
+            style={{ textAlign: 'center', height: 26 }} />} />}
     </View>
-  </View >
+    <ModalApproache visible={visible} toggle={() => setVisible(!visible)} exercise={exercise} />
+  </View>
 }
 
 const styles = StyleSheet.create({
@@ -63,8 +69,8 @@ const styles = StyleSheet.create({
   },
   menu: {
     position: 'absolute',
-    right: 4,
-    top: 10
+    right: 6,
+    top: 5
   }
 })
 
